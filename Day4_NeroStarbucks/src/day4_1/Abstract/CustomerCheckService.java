1a8a4424b4c1f6373302ee668adddc3c04ac266e
Barış TOPAL <79714895+baris-topal@users.noreply.github.com>
2021-05-28 18:40:22 +0300

    Delete Day4_NeroStarbucks directory

--- Day4_NeroStarbucks/src/day4_1/Abstract/CustomerCheckService.java
+++
@@ -1,8 +0,0 @@
-package day4_1.Abstract;
-
-import day4_1.Entities.Customer;
-
-public interface CustomerCheckService {
-
-	boolean checkIfRealPerson(Customer customer);
-}
--- Day4_NeroStarbucks/src/day4_1/Abstract/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package day4_1.Abstract;
-
-import day4_1.Entities.Customer;
-
-public interface CustomerService {
-
-	void save(Customer customer) throws Exception;
-	
-}
